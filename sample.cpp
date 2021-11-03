6af4a9ac7afdaf54aeb4db500c83f217e1c15720
Taha <tahahassanbukhari@gmail.com>
2021-11-03 15:28:47 +0500

    funny program

---
+++ sample.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string name, email;
+	cout << "Please enter your first name \n";
+	cin >> name;
+	cout << "Plaese enter your last name \n";
+	cin >> email;
+//	cout << "You are a stupid person!";
+	
+	if(name == "Taha"){
+		cout << "Output \n";
+		cout << "You are a genius!";
+	}
+	else{
+		cout << "Output \n";
+		cout << "You are a stupid person!";
+	}
+}
+
